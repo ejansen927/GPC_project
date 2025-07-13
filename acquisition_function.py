79de191ca07e861d129fdddf82e72ef68253d3b1
ejansen927 <edwardjansen@mail.adelphi.edu>
2025-07-12 21:31:54 -0400

    Update

--- acquisition_function.py
+++ acquisition_function.py
@@ -32,29 +32,6 @@ def acquisition_function(X_guess, clf, method, num_classes, num_dimensions, X_tr
             Zdiff = Z_sorted[:, -1] - Z_sorted[:, -2]
             return Zdiff
 
-    elif method == 'VR':
-        Z = clf.predict_proba(X_guess)
-        return 1 - np.max(Z, axis=1)
-
-    elif method == 'NormMS':
-        Z = clf.predict_proba(X_guess)
-        Z = np.clip(Z, 1e-12, 1.0)
-        Z_sorted = np.sort(Z, axis=1)
-        margin = Z_sorted[:, -1] - Z_sorted[:, -2]
-        entropy = -np.sum(Z * np.log(Z), axis=1)
-        norm_margin = margin / (entropy + 1e-6)
-        return norm_margin
-
-    elif method == 'ID':
-        if X_train is None:
-            sys.exit("X_train is required for Information Density (ID) method")
-        Z = clf.predict_proba(X_guess)
-        entropy = -np.sum(Z * np.log(Z + 1e-12), axis=1)
-        dists = pairwise_distances(X_guess, X_train)
-        sim = np.exp(-dists / (np.std(dists) + 1e-6))
-        density = np.mean(sim, axis=1)
-        return entropy * density
-
     else:
         sys.exit('No working acquisition function was selected. Choose from: LC, MS, SE, Random-MS, VR, NormMS, ID')
 
